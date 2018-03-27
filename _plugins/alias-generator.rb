80d942e2c97701f93db39becb5828e745186b735
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-03-27 22:29:27 +1300

    add site url, fix alias generator

--- _plugins/alias-generator.rb
+++ _plugins/alias-generator.rb
@@ -2,77 +2,87 @@
 
 module Jekyll
 
-  class AliasGenerator < Generator
+	class PageWithoutAFile < Page
+		def read_yaml(*)
+			@data ||= {}
+		end
+	end
 
-    def generate(site)
-      @site = site
 
-      process_posts()
-      process_pages()
-    end
+	class AliasGenerator < Generator
 
-    def process_posts()
-      @site.posts.docs.each do |post|
-        generate_aliases(post.url, post.data['aliases'])
-      end
-    end
+		def generate(site)
+			@site = site
 
-    def process_pages()
-      @site.pages.each do |page|
-        generate_aliases(page.destination('').gsub(/index\.(html|htm)$/, ''), page.data['aliases'])
-      end
-    end
+			# process_posts() # not sure if this works properly
+			process_pages()
+		end
 
-    def generate_aliases(destination_path, aliases)
-      alias_paths ||= Array.new
-      alias_paths << aliases
-      alias_paths.compact!
+		def process_posts()
+			@site.posts.docs.each do |post|
+				generate_aliases(post, post.url)
+			end
+		end
 
-      alias_paths.flatten.each do |alias_path|
-        alias_path = File.join('/', alias_path.to_s)
+		def process_pages()
+			@site.pages.each do |page|
+				generate_aliases(page, page.path.gsub(/(\/index)?.html$/, ''))
+			end
+		end
 
-        alias_dir  = File.extname(alias_path).empty? ? alias_path : File.dirname(alias_path)
-        alias_file = File.extname(alias_path).empty? ? "index.html" : File.basename(alias_path)
+		def get_template_source_path(file = "alias-template.html")
+			File.expand_path file, __dir__
+		end
 
-        fs_path_to_dir = File.join(@site.dest, alias_dir)
-        alias_sections = alias_dir.split('/')[1..-1]
+		def get_alias_from_template(dest_path, go_to)
+			alias_page = PageWithoutAFile.new(@site, __dir__, "", dest_path)
+			alias_page.content = File.read(get_template_source_path)
+			alias_page.data['layout'] = nil
+			alias_page.data['go_to'] = go_to
+			alias_page
+		end
 
-        FileUtils.mkdir_p(fs_path_to_dir)
+		def generate_aliases(page, go_to)
+			alias_paths ||= Array.new
+			alias_paths << page.data['aliases']
+			alias_paths.compact!
 
-        File.open(File.join(fs_path_to_dir, alias_file), 'w') do |file|
-          file.write(alias_template(destination_path))
-        end
+			alias_paths.flatten.each do |alias_path|
+				alias_path = File.join('/', alias_path.to_s)
 
-        alias_sections.size.times do |sections|
-          @site.static_files << Jekyll::AliasFile.new(@site, @site.dest, alias_sections[0, sections + 1].join('/'), '')
-        end
-        @site.static_files << Jekyll::AliasFile.new(@site, @site.dest, alias_dir, alias_file)
-      end
-    end
+				alias_dir  = File.extname(alias_path).empty? ? alias_path : File.dirname(alias_path)
+				alias_file = File.extname(alias_path).empty? ? "index.html" : File.basename(alias_path)
 
-    def alias_template(destination_path)
-      <<-EOF
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <link rel="canonical" href="#{destination_path}">
-  <meta http-equiv="refresh" content="0;url=#{destination_path}">
-</head>
-</html>
-EOF
-    end
-  end
+				fs_path_to_dir = File.join(@site.dest, alias_dir)
+				alias_sections = alias_dir.split('/')[1..-1]
 
-  class AliasFile < StaticFile
-    require 'set'
+				FileUtils.mkdir_p(fs_path_to_dir)
 
-    def modified?
-      return false
-    end
+				file_path = File.join(fs_path_to_dir, alias_file)
+				alias_page = get_alias_from_template(file_path, go_to)
+				File.open(file_path, 'w') do |file|
+					file.write(alias_page)
+				end
 
-    def write(dest)
-      return true
-    end
-  end
+				@site.pages << alias_page
+
+				alias_sections.size.times do |sections|
+					@site.static_files << Jekyll::AliasFile.new(@site, @site.dest, alias_sections[0, sections + 1].join('/'), '')
+				end
+				@site.static_files << Jekyll::AliasFile.new(@site, @site.dest, alias_dir, alias_file)
+			end
+		end
+	end
+
+	class AliasFile < StaticFile
+		require 'set'
+
+		def modified?
+			return false
+		end
+
+		def write(dest)
+			return true
+		end
+	end
 end
